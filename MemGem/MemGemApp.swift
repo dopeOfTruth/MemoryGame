90bef8b8385d7ae59ff7d3effe1486c612430fb3
Mikhail Krasilnik <brazik.omsk@mail.ru>
2021-02-23 19:16:31 +0600

    Initial Commit

---
+++ MemGem/MemGemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemGemApp.swift
+//  MemGem
+//
+//  Created by Михаил Красильник on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemGemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
